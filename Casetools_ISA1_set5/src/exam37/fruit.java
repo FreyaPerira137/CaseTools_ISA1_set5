b35e11d69ba8d87ff3dafbe82a9744b556d68010
FreyaPerira137 <freyapereira115@gmail.com>
2024-02-12 10:21:23 +0530

    first commit

---
+++ Casetools_ISA1_set5/src/exam37/fruit.java
@@ -0,0 +1,17 @@
+package exam37;
+
+public class fruit {
+	String color;
+	fruit(String color){
+		this.color = color;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		fruit cherry = new fruit ("green");
+		//String color = cherry.color;
+		//System.out.println("Color of cherry is "+color);
+	}
+
+}
